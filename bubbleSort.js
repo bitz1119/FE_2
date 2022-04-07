35223dd252816d8cdeb70affe84135d771fd83a0
Bittoo Aggarwal <bitz1119@gmail.com>
2022-04-07 21:22:47 +0530

    sorting

---
+++ bubbleSort.js
@@ -0,0 +1,18 @@
+let arr = [5,4,3,2,1];
+
+for (let i = 0; i < arr.length-1;i++){
+    // if required swap all subsequent element in array
+    for (let j = 0; j < arr.length-1;j++){
+        // should i swap
+        // if ith number is greater than (i+1)th
+        if(arr[j] > arr[j+1]){
+           // how should i swap
+            let temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+        }
+    }
+}
+
+
+console.log(arr);
