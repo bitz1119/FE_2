a07aafb2d22ccfa4edcf52a7342000c3ac11bb7d
Bittoo Aggarwal <bitz1119@gmail.com>
2022-05-07 11:07:14 +0530

    Stack

--- 05-Stack/stack.js
+++ 05-Stack/stack.js
@@ -1,4 +1,4 @@
-
+``
 class Stack{
     constructor(){
         this.arr = [];
@@ -20,14 +20,55 @@ class Stack{
         return this.arr.length == 0;
     }
 
+    top(){
+        if(this.isEmpty()){
+            throw new Error("Stack is Empty");
+        }
+        return this.arr[this.arr.length - 1];
+    }
+
+    addAtBottom(element){
+        let tempStack = new Stack();
+        while(!s.isEmpty()){
+            tempStack.push(s.pop());
+        }
+        s.push(element);
+        while(!tempStack.isEmpty()){
+            s.push(tempStack.pop());
+        }
+    }
+
+    display(){
+        for(let i=0; i<this.arr.length;i++){
+            console.log(this.arr[i])
+        }
+    }
 }
 
+function reverseStack(stack){
+    if(!stack.isEmpty()){
+        let element = stack.pop()
+        reverseStack(stack);
+        stack.addAtBottom(element);
+    }
+}
+
+
 let s = new Stack();
+
 s.push(10)
-s.push(20)
+s.push(10)
 s.push(30)
-s.push(10)
-s.push(10)
-console.log(s.pop())
-console.log(s.pop())
+s.push(40)
+s.push(50)
+
+s.display()
+
+reverseStack(s);
+s.display()
+
+
+
+
+
 
