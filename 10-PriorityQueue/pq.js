4f5b16c22bb64ad40a40ccf469e7580ddf425c72
Bittoo Aggarwal <bitz1119@gmail.com>
2022-09-08 21:34:07 +0530

    pq

--- 10-PriorityQueue/pq.js
+++ 10-PriorityQueue/pq.js
@@ -32,9 +32,71 @@ class PriorityQueue{
         }
     }
 
+    isLeafNode(parent){
+        if(parent*2+2 > this.pq.length - 1){
+            return true;
+        }
+        return false;
+    }
+
+
+    findHighestAmongParentChild(currIndex){
+        if(this.isLeafNode(currIndex)){
+            return currIndex;
+        }
+
+        let parent = this.pq[currIndex];
+        let leftChild = this.pq[this.leftChild(currIndex)];
+        if(this.rightChild(currIndex) < this.pq.length - 1){
+            let rightChild = this.pq[this.rightChild(currIndex)];
+            let max = Math.max(parent,leftChild,rightChild);
+            if(max === parent){
+                return currIndex;
+            }
+            else if(max === leftChild){
+                return this.leftChild(currIndex);
+            }
+            else {
+                return this.rightChild(currIndex);
+            }
+        }
+        else{
+            let max = Math.max(parent,leftChild);
+            if(max === parent){
+                return currIndex;
+            }
+            else if(max === leftChild){
+                return this.leftChild(currIndex);
+            }
+
+        }
+
+    }
+
+    removeElement(){
+        // swap root->0th Index and Last Index
+        let lastIndex = this.pq.length - 1;
+        let temp = this.pq[lastIndex];
+        this.pq[lastIndex] = this.pq[0];
+        this.pq[0] = temp;
+        
+        // pop last element
+        console.log(this.pq.pop());
+
+        let currIndex = 0;
+        while(!this.isLeafNode(currIndex) && this.findHighestAmongParentChild(currIndex) != currIndex ){        
+            // replace highest with root
+            let highest = this.findHighestAmongParentChild(currIndex);
+            let temp = this.pq[highest];
+            this.pq[highest] = this.pq[currIndex];
+            this.pq[currIndex] = temp;
+            currIndex = highest;
+        }
+    }
+
 
     display(){
-        console.log(pq);
+        console.log(this.pq);
     }
 }
 
@@ -44,5 +106,20 @@ pq.addElement(10);
 pq.addElement(12);
 pq.addElement(3);
 pq.addElement(4);
+
+console.log(pq.findHighestAmongParentChild(3));
+pq.removeElement();
+
+console.log(pq.isLeafNode(0));
+console.log(pq.findHighestAmongParentChild(0));
+
+pq.display();
+
+
+pq.removeElement();
+
+pq.display();
+pq.removeElement();
+
 
 pq.display();
