b57bcd96da94238c5e4ded69c977dc2932a1f207
Bittoo Aggarwal <bitz1119@gmail.com>
2022-09-16 21:36:39 +0530

    adv LL

---
+++ 12-AdvLL/middleEle.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var middleNode = function(head) {
+    let slowPointer = head;
+    let fastPointer = head;
+    
+    while(fastPointer != null && fastPointer.next != null){
+        slowPointer = slowPointer.next;
+        fastPointer = fastPointer.next.next;
+    }
+    
+    return slowPointer;
+};
