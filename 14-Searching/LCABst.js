4fa18899aca12b19123ed6cbb9cce721e1cd3a50
Bittoo Aggarwal <bitz1119@gmail.com>
2022-09-22 22:41:59 +0530

    Searching

---
+++ 14-Searching/LCABst.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+ var lowestCommonAncestor = function(root, p, q) {
+    
+    while(true){
+
+        if(root.val > p.val && root.val > q.val){
+            root = root.left;    
+        }
+    
+        else if(root.val < p.val && root.val < q.val){
+            root = root.right;    
+        }
+        
+        else {
+            return root;
+        }
+    
+    }
+    
+    
+};
